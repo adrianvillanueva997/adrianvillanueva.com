a00e77d065f3dd8cbd51315c0162bedbc7d35b7e
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2021-03-09 17:56:27 +0100

    removed unused code at resume handler

--- src/routes/resume.go
+++ src/routes/resume.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,14 +8,6 @@ import (
 
 func ResumeHandler() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		var posts []string
-		files, err := ioutil.ReadDir(DIRNAME)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, file := range files {
-			posts = append(posts, file.Name())
-		}
 		context.HTML(http.StatusOK, "resume.tmpl.html", gin.H{
 			"title": "Resume",
 		})
