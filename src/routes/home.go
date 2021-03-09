ddba2cd391b7b973f06fe5adb3bae2101a7df40c
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2021-03-09 17:56:05 +0100

    removed unused code at home handler

--- src/routes/home.go
+++ src/routes/home.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,14 +14,6 @@ const DIRNAME = "./src/markdown/"
 
 func IndexHandler() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		var posts []string
-		files, err := ioutil.ReadDir(DIRNAME)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, file := range files {
-			posts = append(posts, file.Name())
-		}
 		context.HTML(http.StatusOK, "index.tmpl.html", gin.H{
 			"title": "Adrián Villanueva",
 		})
