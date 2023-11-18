d487e07bd64c4b2e9f573a3aba3c74f0e804fad5
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-11-18 18:41:02 +0100

    some more stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import { astroImageTools } from 'astro-imagetools';
 import { defineConfig } from 'astro/config';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://adrianvillanueva.com',
+  integrations: [
+    astroImageTools,
+    tailwind(),
+    compress(),
+    sitemap(),
+    robotsTxt(),
+  ],
+});
