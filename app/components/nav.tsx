7f1cede7cf3e83920375058a1acd69c01e31b230
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2025-01-12 20:46:53 +0900

    add icons footer

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -8,7 +8,7 @@ const navItems = {
 		name: "blog",
 	},
 	// "/cv": {
-	// 	name: "deploy",
+	// 	name: "cv",
 	// },
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,20 @@ import { BlogPosts } from "app/components/posts";
 export default function Page() {
   return (
 			<section>
-				<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-					My Portfolio
+				<h1 className="mb-2 text-2xl font-semibold tracking-tighter">
+					Adrian Villanueva Martinez
 				</h1>
+				<div className="w-[50px] h-[2px] bg-neutral-800 dark:bg-neutral-200 mb-8" />
+				<h2 className="mb-4 text-lg font-medium tracking-tight text-neutral-800 dark:text-neutral-200">
+					Senior Software Engineer, currently in 🇯🇵
+				</h2>
 				<p className="mb-4">
-					{`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+					{`I'm a Software Engineer passionate about data engineering, cloud architecture, systems design
+        and technical writing.`}
+				</p>
+				<p className="mb-4">
+					{`I enjoy experimenting with new technologies and finding creative ways
+        to combine them create to cool solutions. This is my personal space on the internet to share my thoughts and technical writings.`}
 				</p>
 				<div className="my-8">
 					<BlogPosts limit={5} />
