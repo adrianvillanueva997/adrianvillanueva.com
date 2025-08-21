5c0b2a0c9c57c5d2e9819989e5ecccdc89921331
Adrián Villanueva <16082421+adrianvillanueva997@users.noreply.github.com>
2025-08-21 12:23:16 +0900

    refactor: Mermaid to d2 (#471)
    * refactor: Mermadi to d2
    
    * update

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -8,12 +8,14 @@ import type { MDXComponents } from "mdx/types";
 import BlogNewsletterForm from "pliny/ui/BlogNewsletterForm";
 import Pre from "pliny/ui/Pre";
 import TOCInline from "pliny/ui/TOCInline";
+import Diagram from './Diagram';
 import Image from "./Image";
 import CustomLink from "./Link";
 import TableWrapper from "./TableWrapper";
 export const components: MDXComponents = {
 	Image,
 	TOCInline,
+	Diagram,
 	a: CustomLink,
 	pre: Pre,
 	table: TableWrapper,
